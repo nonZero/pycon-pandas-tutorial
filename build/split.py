2d88543a0b30e68fbd6ca8f6976b4b3137de70cb
Udi Oron <udioron@gmail.com>
2020-05-03 01:33:02 +0300

    Update some movie names, actors etc. cleanup split.py .

--- build/split.py
+++ build/split.py
@@ -5,40 +5,40 @@ import json
 import os
 import re
 
-def blank_code_cell():
+
+def code_cell(source=None):
     return {
         "cell_type": "code",
         "execution_count": None,
-        "metadata": {
-            "collapsed": True
-        },
+        "metadata": {},
         "outputs": [],
-        "source": [],
+        "source": [] if source is None else source,
     }
+
 
 def question_cell(text):
     return {
         "cell_type": "markdown",
-        "metadata": {
-            "collapsed": True
-        },
+        "metadata": {},
         "source": '### ' + text.strip(),
     }
 
+
 def main():
-    session_cells = {n: [] for n in range(1, 6+1)}
+    session_cells = {n: [] for n in range(1, 6 + 1)}
     f = open(os.path.dirname(os.path.abspath(__file__)) + '/../All.ipynb')
     j = json.load(f)
     cells = j['cells']
     for cell in cells:
-        source = u''.join(cell['source'])
+        source = ''.join(cell['source'])
         m = re.search(r'# +(\d+)\. ', source.strip())
         if not m:
             continue
         n = int(m.group(1))
         session_cells[n].append(cell)
     for n, cells in sorted(session_cells.items()):
-        print 'Session {}: {} cells'.format(n, len(cells))
+        print('Session {}: {} cells'.format(n, len(cells)))
+
 
 def convert(filename):
     f = open(filename)
@@ -48,17 +48,18 @@ def convert(filename):
     with open(filename.replace('Solutions', 'Exercises'), 'w') as f:
         f.write(json.dumps(j, indent=2))
 
+
 def filter_cells(filename, cells):
     n = 0
     starting = True
     for cell in cells:
         if cell['cell_type'] != 'code':
             continue
-        source = u''.join(cell['source'])
+        source = ''.join(cell['source'])
 
         if starting:
             if not source.startswith('# '):
-                yield cell
+                yield code_cell(cell['source'])
             else:
                 starting = False
 
@@ -76,15 +77,17 @@ def filter_cells(filename, cells):
 
         yield question_cell(question)
 
-        yield blank_code_cell()
-        yield blank_code_cell()
+        yield code_cell()
+        yield code_cell()
 
         n += 1
-    print '{:6}   {}'.format(n, filename)
+    print('{:6}   {}'.format(n, filename))
+
 
 def main2():
-    for filename in sorted(glob.glob('Solutions-*.ipynb')):
+    for filename in sorted(glob.glob('../Solutions-*.ipynb')):
         convert(filename)
+
 
 if __name__ == '__main__':
     main2()
